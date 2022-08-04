769557002316ae671935b87d9e18133efb2e7e37
SantiFoglia <Santiago_Foglia@hotmail.com.ar>
2022-08-04 20:16:53 -0300

    branch-MenuPrincipal

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,12 +11,13 @@ public class PlayerController : MonoBehaviour
     private BoxCollider _boxCollider;
 
     //InputTeclas
-
     Dictionary<string, KeyCode> inputTeclas;
 
     //estadisticas
     public float velocidad;
+    float velocidadFija;
     public float velocidadCorriendo;
+    public float velocidadSaltando;
     public float alturaDeSalto;
     public float tiempoAlGirar;
 
@@ -30,6 +31,8 @@ public class PlayerController : MonoBehaviour
     Vector3 velocity;
     public bool tocaPiso;
     public bool estaRolleando;
+    public bool estaCorriendo;
+    public bool estaSaltando;
 
     //flechas
     public GameObject flechaPrefab;
@@ -39,6 +42,7 @@ public class PlayerController : MonoBehaviour
     private float cooldownDisparo = 0.6f;
     private float tiempoRestanteParaAtacar;
     private bool puedeAtacar;
+    public bool estaAtacando;
 
     //apuntar
     public bool estaApuntando = false;
@@ -47,6 +51,13 @@ public class PlayerController : MonoBehaviour
     public Collider[] arrayEnemigos;
     public int objAApuntar = 0;
 
+    //costosStamina
+
+    float costoDisparar = 10f;
+    float costoCorrer = 0.01f;
+    float costoRollear = 20f;
+    float costoSaltar = 20f;
+
 
     private void Start()
     {
@@ -54,8 +65,8 @@ public class PlayerController : MonoBehaviour
         camara = GameObject.FindGameObjectWithTag("MainCamera");
         anim = GetComponent<Animator>();
         _boxCollider = GetComponent<BoxCollider>();
-        //LayerMaskObjAApuntar = LayerMask.GetMask("Enemy");
         CrearinputTeclas();
+        velocidadFija = velocidad;
     }
 
     private void Update()
@@ -82,8 +93,8 @@ public class PlayerController : MonoBehaviour
         inputTeclas = new Dictionary<string, KeyCode>();
 
         inputTeclas.Add("saltar", KeyCode.Space);
-        inputTeclas.Add("rollear", KeyCode.LeftShift);
-        inputTeclas.Add("correr", KeyCode.LeftControl);
+        inputTeclas.Add("rollear", KeyCode.LeftControl);
+        inputTeclas.Add("correr", KeyCode.LeftShift);
         inputTeclas.Add("disparar", KeyCode.Mouse0);
         inputTeclas.Add("apuntar", KeyCode.Mouse1);
         inputTeclas.Add("cambiarObjD", KeyCode.E);
@@ -98,28 +109,43 @@ public class PlayerController : MonoBehaviour
         {
             velocity.y = -2f;
             anim.SetBool("salto", false);
+
+        }
+
+        if (!tocaPiso)
+        {
+            estaSaltando = true;
+            velocidad = velocidadSaltando;
+        }
+        else
+        {
+            estaSaltando = false;
+            velocidad = velocidadFija;
         }
     }
     void Roll()
     {
-        if (Input.GetKeyDown(inputTeclas["rollear"]) && tocaPiso)
+        if (Input.GetKeyDown(inputTeclas["rollear"]) && tocaPiso && Jugador.stamina>costoRollear)
         {
             StartCoroutine(tiempoAnimacionRoll());
             _boxCollider.enabled = false;
             estaRolleando = true;
             puedeAtacar = false;
             anim.SetTrigger("roll");
+
+            Jugador.stamina -= costoRollear;
         }
 
 
     }
     void Salto()
     {
-        if (Input.GetKeyDown(inputTeclas["saltar"]) && tocaPiso)
+        if (Input.GetKeyDown(inputTeclas["saltar"]) && tocaPiso && Jugador.stamina > costoSaltar && !estaRolleando)
         {
-
             velocity.y = Mathf.Sqrt(alturaDeSalto * -2 * gravedad);
             anim.SetBool("salto", true);
+
+            Jugador.stamina -= costoSaltar;
 
         }
     }
@@ -143,21 +169,26 @@ public class PlayerController : MonoBehaviour
                 float angulo = Mathf.SmoothDampAngle(transform.eulerAngles.y, objetivoAngulo, ref velocidadGiro, tiempoAlGirar);
                 transform.rotation = Quaternion.Euler(0, angulo, 0);
 
-                if (Input.GetKey(inputTeclas["correr"]) && !estaApuntando)
+                if (Input.GetKey(inputTeclas["correr"]) && !estaApuntando && Jugador.stamina > costoCorrer && !estaAtacando)
                 {
                     //ahora se va a mover hacia el lado donde giramos
                     Vector3 mover = Quaternion.Euler(0, objetivoAngulo, 0) * Vector3.forward;
                     controller.Move(mover.normalized * velocidadCorriendo * Time.deltaTime);
 
                     anim.SetFloat("Movimientos", 1f, 0.1f, Time.deltaTime);
+
+                    estaCorriendo = true;
+                    Jugador.stamina -= costoCorrer;
                 }
-                else
+                else if(!estaAtacando)
                 {
                     //ahora se va a mover hacia el lado donde giramos
                     Vector3 mover = Quaternion.Euler(0, objetivoAngulo, 0) * Vector3.forward;
                     controller.Move(mover.normalized * velocidad * Time.deltaTime);
 
                     anim.SetFloat("Movimientos", 0.5f, 0.1f, Time.deltaTime);
+
+                    estaCorriendo = false;
                 }
 
 
@@ -166,12 +197,14 @@ public class PlayerController : MonoBehaviour
     }
     void Disparar()
     {
-        if (Input.GetKeyDown(inputTeclas["disparar"]) && puedeAtacar && tocaPiso && !estaRolleando)
+        if (Input.GetKeyDown(inputTeclas["disparar"]) && puedeAtacar && tocaPiso && !estaRolleando && Jugador.stamina > costoDisparar)
         {
             puedeAtacar = false;
+            estaAtacando = true;
             tiempoRestanteParaAtacar = cooldownDisparo;
             anim.SetTrigger("ataque");
             StartCoroutine(tiempoAnimacionAtaque());
+            Jugador.stamina -= costoDisparar;
 
         }
     }
@@ -248,9 +281,11 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator tiempoAnimacionAtaque()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.4f);
         flecha = Instantiate(flechaPrefab, spawnFlecha.position, spawnFlecha.rotation);
         flecha.GetComponent<Rigidbody>().AddForce(spawnFlecha.forward * 50, ForceMode.Impulse);
+        estaAtacando = false;
+
     }
 
     IEnumerator tiempoAnimacionRoll()
